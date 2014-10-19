79981350a2a9122d1833b082dacb186256a2f739
smusings <etyulmenkov@yahoo.com>
2014-10-19 15:00:55 -0400

    ex9

---
+++ ex1-3/ex9.sql
@@ -0,0 +1,11 @@
+UPDATE person SET first_name = "Eugene"
+	WHERE first_name = "Hilarious Guy";
+	
+UPDATE pet SET name = "Tulouse"
+	WHERE id = 0;
+	
+UPDATE pet SET name = "Deceased"
+	WHERE dead = 1;
+	
+SELECT * FROM person;
+SELECT * FROM pet;
